abae3398f4cc4805f171de4d4b2c6307c9a1f5dc
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-12-25 10:49:51 +0300

    Initial Commit

---
+++ Movie for todayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie for todayApp
+//
+//  Created by Владислав on 25.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
