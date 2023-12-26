a33365403c26cceac0af0a93d4efb9b393ab0742
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-12-26 13:22:25 +0300

    add tabbar

--- Movie for todayApp/Application/SceneDelegate.swift
+++ Movie for todayApp/Application/SceneDelegate.swift
@@ -13,11 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = HomeViewController()
-        window?.makeKeyAndVisible()
-    }
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            window = UIWindow(windowScene: windowScene)
+            window?.rootViewController = CustomTabBarController()
+            window?.makeKeyAndVisible()
+        }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
