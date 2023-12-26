d1948ef403b8ee34797f0408076fea9b459ad3c4
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-12-26 11:17:21 +0200

    create init VIP layout

--- Movie for todayApp/Scenes/Profile/ProfileViewController.swift
+++ Movie for todayApp/Scenes/Profile/ProfileViewController.swift
@@ -5,4 +5,58 @@
 //  Created by Владислав on 25.12.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol ProfileDisplayLogic: AnyObject {
+    func displayProfile()
+}
+
+class ProfileViewController: UIViewController {
+    // MARK: - UI components
+    
+    // MARK: - Internal vars
+    
+    // MARK: - External vars
+    private weak var interactor: ProfileInteractionLogic?
+    // MARK: - Initializing
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setupVC()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupVC()
+    }
+    
+    private func setupVC() {
+        let presenter = ProfilePresenter()
+        let interactor = ProfileInteractor()
+        
+        presenter.viewController = self
+        interactor.presenter = presenter
+        self.interactor = interactor
+    }
+    
+    // MARK: - View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+// MARK: - Display logic
+extension ProfileViewController: ProfileDisplayLogic {
+    func displayProfile() {
+        // TODO: Display profile
+    }
+}
+// MARK: - Setup UI
+extension ProfileViewController {
+    
+    func setupView() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+}
