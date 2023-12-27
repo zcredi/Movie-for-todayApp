cc0bf671de94b4a15df43336d0a28a12d1be9a0a
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-12-27 13:00:32 +0200

    add table view and profile details cells

---
+++ Movie for todayApp/Scenes/Profile/ProfileDetailsTableViewCell.swift
@@ -0,0 +1,77 @@
+//
+//  ProfileDetailsTableViewCell.swift
+//  Movie for todayApp
+//
+//  Created by Andrei Shpartou on 27/12/2023.
+//
+
+import UIKit
+
+class ProfileDetailsTableViewCell: UITableViewCell {
+    static let identifier = "ProfileDetailsTableViewCell"
+    
+    private lazy var iconContainer: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        //imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        //label.text = nil
+        //label.font =
+        //label.textColor
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(iconContainer)
+        iconContainer.addSubview(iconImageView)
+        contentView.addSubview(label)
+        contentView.clipsToBounds = true
+        accessoryType = .disclosureIndicator
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size = contentView.frame.size.height - 12
+        iconContainer.frame = CGRect(x: 10, y: 6, width: size, height: size)
+        
+        let imageSize = size / 1.5
+        iconImageView.frame = CGRect(x: (size - imageSize) / 2, y: (size - imageSize) / 2, width: imageSize, height: imageSize)
+        
+        label.frame = CGRect(x: 30 + Int(iconContainer.frame.size.width),
+                             y: 0,
+                             width: Int(contentView.frame.size.width - 30 - iconContainer.frame.size.width),
+                             height: Int(contentView.frame.size.height))
+                             
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconImageView.image = nil
+        label.text = nil
+        iconContainer.backgroundColor = nil
+    }
+    
+    func configure(with model: ProfileDetailsOption) {
+        label.text = model.title
+        iconImageView.image = model.image
+        iconContainer.backgroundColor = model.imageBackGroundColor
+    }
+    
+}
--- Movie for todayApp/Scenes/Profile/ProfileViewController.swift
+++ Movie for todayApp/Scenes/Profile/ProfileViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 protocol ProfileDisplayLogic: AnyObject {
     func displayProfile()
 }
+// MARK: - Profile Model
 
-class ProfileViewController: UIViewController {
-    // MARK: - UI components
-    
-    // MARK: - Internal vars
-    
-    // MARK: - External vars
+struct ProfileSection {
+    let title: String
+    let options: [ProfileDetailsOption]
+}
+
+struct ProfileDetailsOption {
+    let title: String
+    let image: UIImage?
+    let imageBackGroundColor: UIColor
+    let handler: (() -> Void)
+}
+// MARK: - Profile View Controller
+class ProfileViewController: UIViewController, UITableViewDelegate {
+    // UI components
+    private lazy var tableView = createTableView()
+    // Internal vars
+    private var models = [ProfileSection]()
+    // External vars
     private weak var interactor: ProfileInteractionLogic?
     // MARK: - Initializing
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -41,8 +54,14 @@ class ProfileViewController: UIViewController {
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
     }
 }
+// MARK: - ProfileTableViewCell type
+//class ProfileTableViewCell: UITableViewCell {
+//    func setup(with viewModel)
+//}
 // MARK: - Display logic
 extension ProfileViewController: ProfileDisplayLogic {
     func displayProfile() {
@@ -52,11 +71,114 @@ extension ProfileViewController: ProfileDisplayLogic {
 // MARK: - Setup UI
 extension ProfileViewController {
     
-    func setupView() {
-        
+    // MARK: - UI Elements
+    private func createTableView() -> UITableView {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+//        tableView.register(ProfileDetailsTableViewCell.self,
+//                           forCellReuseIdentifier: ProfileDetailsTableViewCell.identifier)
+        tableView.register(ProfileDetailsTableViewCell.self,
+                                   forCellReuseIdentifier: ProfileDetailsTableViewCell.identifier)
+        //tableView.backgroundColor = UIColor.primaryDark
+        return tableView
+    }
+    // MARK: - UI methods
+    private func setupUI() {
+        configure()
+        setupView()
+        setupConstraints()
     }
     
-    func setupConstraints() {
-        
+    private func configure() {
+        //General section
+        self.models.append(ProfileSection(title: "General", options: [
+            ProfileDetailsOption(title: "Notification",
+                                 image: UIImage(systemName: "bell.fill"),
+                                 imageBackGroundColor: UIColor.primaryDark,
+                                 handler: {
+                                     print("Notification cell tapped")
+                                 }),
+            ProfileDetailsOption(title: "Language",
+                                 image: UIImage(systemName: "pencil"),
+                                 imageBackGroundColor: UIColor.primaryDark,
+                                 handler: {
+                                     print("Language cell tapped")
+                                 })
+        ]))
+        //More section
+        self.models.append(ProfileSection(title: "More", options: [
+            ProfileDetailsOption(title: "Legal and Policies",
+                                 image: UIImage(systemName: "list.bullet"),
+                                 imageBackGroundColor: UIColor.primaryDark,
+                                 handler: {
+                                     print("More cell tapped")
+                                 }),
+            ProfileDetailsOption(title: "About Us",
+                                 image: UIImage(systemName: "pencil.tip.crop.circle.badge.plus"),
+                                 imageBackGroundColor: UIColor.primaryDark,
+                                 handler: {
+                                     print("About Us cell tapped")
+                                 })
+        ]))
+    }
+    
+    private func setupView() {
+        title = "Profile"
+        view.backgroundColor = UIColor.primaryDark
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    // MARK: - Constraints
+    private func setupConstraints() {
+        tableView.frame = view.bounds
     }
 }
+// MARK: - TableView data source
+extension ProfileViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        models.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = models[section]
+        return section.title
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section].options[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: ProfileDetailsTableViewCell.identifier,
+            for: indexPath
+        ) as? ProfileDetailsTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = models[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    
+}
+
+
+ //MARK: - Preview
+//import SwiftUI
+//struct MainViewPreviews: PreviewProvider {
+//    static var previews: some View {
+//        SwiftUIHelloWorldView()
+//    }
+//}
+//struct SwiftUIHelloWorldView: UIViewRepresentable {
+//    func makeUIView(context: Context) -> UIView {
+//        return ProfileViewController().view
+//    }
+//    func updateUIView(_ view: UIView, context: Context) {}
+//}
