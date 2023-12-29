d3f5ff279ce019f01a5b46b0fcf12242d2a69d56
Alexander Altman <qewhouse@gmail.com>
2023-12-30 02:45:28 +0300

    basic collection

--- Movie for todayApp/Scenes/Onboarding/OnboardingViewController.swift
+++ Movie for todayApp/Scenes/Onboarding/OnboardingViewController.swift
@@ -5,4 +5,59 @@
 //  Created by Владислав on 25.12.2023.
 //
 
-import Foundation
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    lazy var onboardingCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: view.frame.width, height: view.frame.height)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.scrollDirection = .horizontal
+        
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: String(describing: UICollectionViewCell.self))
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.isPagingEnabled = true
+//        collection.showsHorizontalScrollIndicator = false
+        
+        return collection
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        view.addSubview(onboardingCollectionView)
+        
+        NSLayoutConstraint.activate([
+            onboardingCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            onboardingCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            onboardingCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            onboardingCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        
+        ])
+    }
+    
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: UICollectionViewCell.self), for: indexPath)
+        cell.contentView.backgroundColor = .green.withAlphaComponent(0.5)
+        return cell
+    }
+    
+    
+}
