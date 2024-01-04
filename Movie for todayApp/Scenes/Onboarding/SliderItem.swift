cccfccc711a7b8dd841aeb05af21ee4173723ce3
Vladislav <107634092+zcredi@users.noreply.github.com>
2024-01-04 15:05:22 +0300

    add onboarding

---
+++ Movie for todayApp/Scenes/Onboarding/SliderItem.swift
@@ -0,0 +1,14 @@
+//
+//  SliderItem.swift
+//  Movie for todayApp
+//
+//  Created by Alexander Altman on 02.01.2024.
+//
+
+import Foundation
+
+struct SliderItem {
+    var title: String
+    var text: String
+    var animationName: String
+}
